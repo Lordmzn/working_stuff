a7398196b0c52b4eb73bf93808b53262bc37295e
Emanuele Mason <lordmzn@gmail.com>
2013-10-09 15:33:15 +0200

    SIMO proj repo test

---
+++ it.polimi.dei.SIMO/include/PlaceHolder.hpp
@@ -0,0 +1,21 @@
+/*
+ * PlaceHolder.hpp
+ *
+ *  Created on: 09/ott/2013
+ *      Author: emanuele
+ */
+
+#ifndef PLACEHOLDER_HPP_
+#define PLACEHOLDER_HPP_
+
+namespace it_polimi_dei_SIMO {
+
+class PlaceHolder {
+public:
+	PlaceHolder();
+	virtual ~PlaceHolder();
+};
+
+} /* namespace it_polimi_dei_SIMO */
+
+#endif /* PLACEHOLDER_HPP_ */
---
+++ it.polimi.dei.SIMO/test/PlaceHolder_test.cpp
@@ -0,0 +1,12 @@
+/*
+ * PlaceHolder_test.cpp
+ *
+ *  Created on: 09/ott/2013
+ *      Author: emanuele
+ */
+
+#include "PlaceHolder.hpp"
+
+namespace it_polimi_dei_SIMO {
+
+} /* namespace it_polimi_dei_SIMO */
